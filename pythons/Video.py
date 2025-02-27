2302ee9e94bf7864cdb54fa02cb7fcde171514a4
LJFYC007 <ljfcnyali@gmail.com>
2025-02-27 13:14:44 +0800

    Complete video generation as openeb

--- pythons/Video.py
+++ pythons/Video.py
@@ -4,6 +4,7 @@ import subprocess
 import cv2
 from glob import glob
 import numpy as np
+from tqdm import tqdm
 
 work_dir = r"C:\Users\-LJF007-\Documents"
 output_dir = os.path.join(work_dir, "output", "BistroInterior_Wine")
@@ -12,6 +13,8 @@ scene_path = os.path.join(work_dir, "Scenes", "Bistro_v5_2", "BistroInterior_Win
 
 video_width = 1280
 video_height = 720
+fps = 60
+time_window_ms = 1000.0 / fps
 
 def run_mogwai():
     os.chdir(work_dir)
@@ -21,25 +24,40 @@ def run_mogwai():
 def create_video_from_exr():
     exr_files = sorted(glob(os.path.join(output_dir, ".ErrorMeasurePass.Output.*.exr")),
                        key=lambda x: int(x.split('.')[-2]))
+
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     video_output = os.path.join(work_dir, "output", "BistroInterior_Wine.mp4")
-    video_writer = cv2.VideoWriter(video_output, fourcc, 500, (video_width, video_height))
+    video_writer = cv2.VideoWriter(video_output, fourcc, fps, (video_width, video_height))
 
-    for exr_file in exr_files:
+    events = []
+    for exr_file in tqdm(exr_files, desc="Processing EXR files"):
         frame = cv2.imread(exr_file, cv2.IMREAD_UNCHANGED)
-
         if frame is None:
             print(f"Failed to read {exr_file}")
             continue
-
         if frame.dtype == np.float32:
             frame = cv2.normalize(frame, None, 0, 255, cv2.NORM_MINMAX)
             frame = np.uint8(frame)
+        events.append(frame)
 
-        frame_resized = cv2.resize(frame, (video_width, video_height))
-        video_writer.write(frame_resized)
+        if len(events) * 1 >= time_window_ms:
+            latest_event = np.zeros((frame.shape[0], frame.shape[1]), dtype=np.uint8)
+
+            for e in events:
+                bright_event = (e[:, :, 2] >= 200)
+                dark_event = (e[:, :, 1] >= 200)
+                latest_event[bright_event] = 1
+                latest_event[dark_event] = 2
+
+            visualization_frame = np.ones((frame.shape[0], frame.shape[1], 3), dtype=np.uint8) * 255 # White
+            visualization_frame[latest_event == 1] = [0.78 * 255, 0.49 * 255, 0.25 * 255] # Blue
+            visualization_frame[latest_event == 2] = [0.20 * 255, 0.14 * 255, 0.11 * 255] # Black
+
+            video_writer.write(visualization_frame)
+            events.clear()
 
     video_writer.release()
+    print(f"Video saved at: {video_output}")
 
 # run_mogwai()
 create_video_from_exr()
