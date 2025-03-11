beaac696ed3260406857e64105deb4de37109f2c
LJFYC007 <ljfcnyali@gmail.com>
2025-03-11 12:42:12 +0800

    GT

--- network/dataset.py
+++
@@ -1,4 +0,0 @@
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
-
-class EventDataset(Dataset):
--- network/train.py
+++
@@ -1,6 +0,0 @@
-from dataset import EventDatase
-from torch.utils.data import DataLoader
-
-def train(args):
-    dataset = EventDataset('..\\Data')
-    dataloader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=8)
